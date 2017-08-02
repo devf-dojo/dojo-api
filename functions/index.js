9c128c9996b85c1e35ac4683ce182924032284fa
Ernesto Ramirez <NearTox@outlook.com>
2017-08-01 21:38:04 -0500

    add express

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,27 @@
 const functions = require('firebase-functions');
-const userModel = require('./userModel');
-const db = require("./db")
 const admin = require('firebase-admin');
+const cors = require('cors');
+const express = require('express');
 
+const userModel = require('./userModel');
+const db = require("./db");
 
+// init the admin
 const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp(
-{
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://devf-dojo-admin.firebaseio.com"
 });
 
-exports.getUser = functions.https.onRequest((req, res) => {
-  if(req.method != "POST") {
+// init express
+const app = express()
+app.use(cors({ origin: true }))
+/*app.get("*", (request, response) => {
+  response.send("Hello from Express on Firebase with CORS! No trailing slash required!")
+})*/
+app.use('/v1/dojo/get_user', (req, res, next) => {
+ if(req.method != "POST") {
     res.json(405, {
       error: {
         code: "method not allowed",
@@ -27,22 +35,27 @@ exports.getUser = functions.https.onRequest((req, res) => {
   db.getUser(user, (user_info) => {
     if(user_info["error"] !== undefined) {
       res.json(403, user_info)
-    } else {
-    	var uid = user_info.uid;
-    	
-    	admin.auth().createCustomToken(uid)
-    	.then(function(customToken){
-    		res.json({uid:uid, jwt: customToken})
-    	})
-    	.catch(function(error) {
-		    console.log("Error creating custom token:", error);
-		  });
-      	
+      return;
     }
+    var uid = user_info.uid;
+    //res.json(user_info)
+
+    admin.auth().createCustomToken(uid).then((customToken) => {
+      res.json({uid: uid, jwt: customToken})
+    }).catch((error) => {
+      console.log("Error creating custom token:", error);
+
+      res.json(400, {
+        error: {
+          code: "Error creating custom token",
+          message: error
+        }
+      })
+    })
   });
 })
 
-exports.usercv = functions.https.onRequest((req, res) => {
+app.use('/v1/dojo/is_cv_valid', (req, res, next) => {
   const data = req.body;
   const is_valid = userModel.validateCvUser(data)
   if(!is_valid) {
@@ -55,4 +68,11 @@ exports.usercv = functions.https.onRequest((req, res) => {
     return;
   }
   res.json(data)
+})
+
+exports.api = functions.https.onRequest((request, response) => {
+  if (!request.path) {
+    request.url = `/${request.url}` // prepend '/' to keep query params if any
+  }
+  return app(request, response)
 })
